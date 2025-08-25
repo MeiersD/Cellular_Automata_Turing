d5a94b872a5e0093323c984b125081abac5bc630
MeiersD <meiersdixon@gmail.com>
2025-08-24 22:10:35 -0400

    changed greek letters for lower case letters since greeks are 16bits

--- doLogic.c
+++ doLogic.c
@@ -1,34 +1,40 @@
 #include "translate.c"
 #include <stdio.h>
+#include <string.h>
 
+void translateAndPrint(char*);
 char* iterate(char*);
 
-//Do not ask what any of these mean, I got this from Table3 in the paper
+// Because I had the enormous forsight to make this app in C, I need to make all state names a single character so they can be represented on the tape
 enum State {
-    a,
-    Q0,
-    dig,
-    zle,
-    ole,
-    zad,
-    oad,
-    car,
-    new,
-    rig,
-    lef,
-    fin,
-    halt
+    a = 'a', //a
+    b = 'b', //Q0 this is the starting state
+    c = 'c', //dig
+    d = 'd', //zle
+    e = 'e', //ole
+    f = 'f', //zad
+    g = 'g', //oad
+    h = 'h', //car
+    i = 'i', //new 
+    j = 'j', //rig
+    k = 'k', //lef
+    l = 'l', //fin
+    m = 'm' //halt ends the binary addition program
 };
+
+enum State state = b;
+int headPos = 2;
 
 /*
  * The plan is to make a function which calls itself recursivly and prints out the current row each time. Params: Starting string
  */
 char* iterate(char* currString){
     // Base case
-    // if (state = "halt"){return;}
-    //
+    if (state == m){return currString;}
+    char* nextString = currString; // nextString starts as a copy of currString, then gets modified based on current state and headPos
     //
     // change state
     // print out nextString string
+    translateAndPrint(nextString);
     // iterate(nextString)
 }
